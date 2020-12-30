d3dc1eb2a7819513f674c999ae5c558076e825dc
Emil Sunesson <sunesson.emil@gmail.com>
2020-12-30 14:27:45 +0100

    Added rfid support

--- vend/CardAdmin.ino
+++ vend/CardAdmin.ino
@@ -1,34 +1,89 @@
-void addCard(char value[]) {
-  uint8_t currAddr = EEPROM.read(0);
-  if (currAddr > 254) {
-    currAddr = 1;
+void addCard(uint8_t value[]) {
+  if (!getCard(value)) {
+    Serial.println("Card not found adding...");
+
+    uint8_t currAddr = EEPROM.read(0);
+    if (currAddr > 254) {
+      currAddr = 1;
+    }
+
+    Serial.print("Current nr of cards is: ");
+    Serial.print(currAddr);
+    Serial.print("\n");
+
+    currAddr++;
+
+    int addr = currAddr * 4;
+
+    for (int i = 0; i < 4; i++) {
+      uint8_t cval = value[i];
+      EEPROM.write(addr + i, cval);
+    }
+
+    EEPROM.write(0, currAddr);
+
+    Serial.print("Card with id: ");
+    printId(value);
+    Serial.print(" added... \n");
+  } else {
+    Serial.println("Card already exists...");
   }
-
-  int addr = currAddr * 4;
-  Serial.println(currAddr);
-
-  currAddr++;
-
-  for (int i = 0; i < 4; i++) {
-    uint8_t cval = value[i] - 0;
-    EEPROM.write(addr + i, cval);
-  }
-
-  EEPROM.write(0, currAddr);
 }
 
-bool getCard(char value[]) {
+bool getCard(uint8_t value[]) {
   uint8_t addr = EEPROM.read(0);
 
   bool result = false;
 
   for (int i = 4; i <= addr * 4; i = i + 4) {
-    if (value[0] - 0 == EEPROM.read(i) && value[1] - 0 == EEPROM.read(i + 1) && value[2] - 0 == EEPROM.read(i + 2) && value[3] - 0 == EEPROM.read(i + 3)) {
+
+    if (value[0] == EEPROM.read(i) && value[1] == EEPROM.read(i + 1) && value[2] == EEPROM.read(i + 2) && value[3] == EEPROM.read(i + 3)) {
       result = true;
       break;
     }
   }
 
   return result;
+}
 
+void listEEProm() {
+  uint8_t addr = EEPROM.read(0);
+
+  for (int i = 4; i <= addr * 4; i = i + 4) {
+
+    uint8_t id[5];
+
+    id[0] = EEPROM.read(i);
+    id[1] = EEPROM.read(i + 1);
+    id[2] = EEPROM.read(i + 2);
+    id[3] = EEPROM.read(i + 3);
+    id[4] = '\0';
+
+    printId(id);
+    Serial.println();
+  }
+}
+
+void clearEEProm() {
+  Serial.println("Clearing all EEprom memory");
+
+  for (int i = 0 ; i < EEPROM.length() ; i++) {
+    EEPROM.write(i, 0);
+  }
+}
+
+void printId(uint8_t value[]) {
+  for (byte j = 0; j < 4; j++)
+    {
+      Serial.print(value[j] < 0x10 ? " 0" : " ");
+      Serial.print(value[j], HEX);
+    }
+}
+
+bool compareId(uint8_t a[], uint8_t b[]) {
+  if(a[0] == b[0] && a[1] == b[1] && a[2] == b[2] && a[3] == b[3]) {
+    return true;
+  }
+
+  return false;
 }
--- vend/vend.ino
+++ vend/vend.ino
@@ -1,3 +1,8 @@
+#include <deprecated.h>
+#include <MFRC522.h>
+#include <MFRC522Extended.h>
+#include <require_cpp11.h>
+
 #include <SPI.h>
 #include <EEPROM.h>
 #include <Wire.h>
@@ -5,6 +10,8 @@
 #include "PN532_SPI.h"
 #include "LiquidCrystal_I2C.h"
 
+#define SS_PIN 10
+#define RST_PIN 9
 
 #define PRICE1RELAYPIN 7
 #define PRICE2RELAYPIN 6
@@ -14,13 +21,18 @@
 //A0-A5 LCD
 
 //#define DEBUG
-
+MFRC522 mfrc522(SS_PIN, RST_PIN);
 PN532_SPI pn532spi(SPI, 10);
 PN532* pn532 = new PN532(pn532spi);
 LiquidCrystal_I2C lcd(0x27, 16, 2); // set the LCD address to 0x27 for a 16 chars and 2 line display
 
-char data[5];
-boolean newData = false;
+uint8_t data[5];
+bool newData = false;
+unsigned long adminScanTime = 0;
+int adminScanDelay = 10000;
+uint8_t adminCard[4] = {177, 177, 177, 177};
+uint8_t clearCommand[4] = {173, 227, 236, 242};
+uint8_t listCommand[4] = {173, 236, 243, 244};
 
 void setup(void) {
   Serial.begin(57600);
@@ -31,6 +43,8 @@ void setup(void) {
   pinMode(PRICE1RELAYPIN, OUTPUT);
   pinMode(PRICE1SENSEPIN, INPUT_PULLUP);
 
+  SPI.begin();
+  mfrc522.PCD_Init();
 
   lcd.init();                      // initialize the lcd
 
@@ -48,22 +62,14 @@ void loop(void) {
   }
 
   readSerialBuffer();
+  scanRFID();
 
   if (newData == true) {
-    Serial.println(data);
-    addCard(data);
+
+    handleInput();
+
     newData = false;
-
-    char test[4];
-
-    test[0] = '1';
-    test[1] = '2';
-    test[2] = '3';
-    test[3] = '8';
-    
-    Serial.println(getCard(test));
   }
-
 
   //    digitalWrite(PRICE1RELAYPIN, HIGH);
 
@@ -71,11 +77,49 @@ void loop(void) {
   //    if(digitalRead(PRICE1SENSEPIN) == HIGH){
   //      // do stuff
   //    }
+  delay(500);
+}
 
+void handleInput() {
+  if (compareId(data, adminCard)) {
+    Serial.println("Admin card scanned...");
+
+    adminScanTime = millis();
+  } else {
+    if (compareId(data, clearCommand) && adminCardIsScaned()) {
+      clearEEProm();
+      adminScanTime = 0;
+    } else if (compareId(data, listCommand) && adminCardIsScaned()) {
+      listEEProm();
+      adminScanTime = 0;
+    } else {
+      checkOrAddCard();
+    }
+  }
+}
+
+void checkOrAddCard() {
+  if (getCard(data)) {
+        // Valid card do some stuff
+        Serial.print("Card is valid id is: ");
+        printId(data);
+        Serial.print("\n");
+
+      } else {
+        if (adminCardIsScaned()) {
+          addCard(data);
+          adminScanTime = 0;
+        } else {
+          Serial.println("Card not found scan admin card to add...");
+        }
+      }
+}
+
+bool adminCardIsScaned() {
+  return (adminScanTime + adminScanDelay) > millis() && adminScanTime > adminScanDelay;
 }
 
 void readSerialBuffer() {
-
   if (Serial.available() > 0 && newData == false) {
     char buff[32];
 
@@ -91,10 +135,36 @@ void readSerialBuffer() {
       i++;
     }
 
-    data[0] = buff[0];
-    data[1] = buff[1];
-    data[2] = buff[2];
-    data[3] = buff[3];
+    if (i != 4) {
+      newData = false;
+      Serial.println("Corrupt read");
+      return;
+    }
+
+    data[0] = buff[0] + 128;
+    data[1] = buff[1] + 128;
+    data[2] = buff[2] + 128;
+    data[3] = buff[3] + 128;
   }
+}
+
+void scanRFID() {
+  // Look for new cards
+  if ( ! mfrc522.PICC_IsNewCardPresent())
+  {
+    return;
+  }
+  // Select one of the cards
+  if ( ! mfrc522.PICC_ReadCardSerial())
+  {
+    return;
+  }
+
+  newData = true;
+
+  data[0] = mfrc522.uid.uidByte[0];
+  data[1] = mfrc522.uid.uidByte[1];
+  data[2] = mfrc522.uid.uidByte[2];
+  data[3] = mfrc522.uid.uidByte[3];
 
 }
